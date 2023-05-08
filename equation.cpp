f7669d76ce4b84bd66eb258e1ee8deb4fd072477
徐靖 <43105086+freewillbe@users.noreply.github.com>
2023-05-08 15:26:02 +0800

    Update equation.cpp

--- equation.cpp
+++ equation.cpp
@@ -1,1 +1,5 @@
-
+#include <iostream>
+using namespace std;
+                                    int main()
+{                                           // to solve 3x + 2 = 11
+        int coefficient, term_b, term_c;                                        cout << "Input coefficient of x, term b and term c.\n";                 cin >> coefficient >> term_b >> term_c;                                                                     cout << "x = " << term_c - term_b << " / " << coefficient;              cout << endl;               }
